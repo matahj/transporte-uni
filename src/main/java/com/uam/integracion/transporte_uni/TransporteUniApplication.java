afdf00b8367706c649448fc709c1cad4d71042f2
JorgeMataH <matahj@gmail.com>
2025-03-28 14:29:44 -0600

    first commit

---
+++ src/main/java/com/uam/integracion/transporte_uni/TransporteUniApplication.java
@@ -0,0 +1,13 @@
+package com.uam.integracion.transporte_uni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransporteUniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransporteUniApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uam/integracion/transporte_uni/TransporteUniApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uam.integracion.transporte_uni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteUniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
